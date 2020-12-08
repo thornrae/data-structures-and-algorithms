fde342f499be03bef4568ebbe31b1ff5b2e028cc
Taylor Thornton <thornrae@gmail.com>
2020-12-07 21:21:53 -0800

    challenge 2 completed

--- code-challenges/challenges-01.test.js
+++ code-challenges/challenges-01.test.js
@@ -37,11 +37,10 @@ const addValues = (arr, value) => {
 };
 
 const addNumbers = (num, arr, times, callback) => {
- const number = callback(num);
- 
- 
-
- return arr 
+  for (var i = 0; i<times; i++){
+    callback(arr,num);
+  }
+  return arr;
 };
 
 /* ------------------------------------------------------------------------------------------------
