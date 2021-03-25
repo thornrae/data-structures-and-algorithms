e6a2743b98071de1fa439066ee25328b862e0178
Taylor Thornton <thornrae@gmail.com>
2021-03-25 16:47:48 -0700

    enqueue test passing

--- javascript/linked-list/__tests__/queue-test.js
+++ javascript/linked-list/__tests__/queue-test.js
@@ -2,3 +2,21 @@
 
 const Queue = require('../../stacksAndQueues/queue.js');
 const Node = require('../lib/node.js');
+
+describe('**** QUEUES ****', () => {
+  //enqueue
+  it('should create new node and push to queue storage', () => {
+    let queue = new Queue();
+    let string = 'test value'
+
+    queue.enqueue(string);
+
+   let node = new Node(string);
+
+    expect(queue.storage).toContainEqual(node);
+  })
+
+  //dequeue
+  it('should ')
+
+})
---
+++ javascript/stacksAndQueues/README.md
@@ -0,0 +1,19 @@
+# Stacks and Queues
+Stack Methods Given: push, pop, peek.
+
+Queue Methods Given: enqueue, dequeue, peek
+
+## Challege
+For both Stack and Queue create a method:
+isEmpty - returns true if stack is empty, returns false if stack is not empty
+
+## Approach & Efficiency
+Push: O(1)
+Pop: O(1)
+Peek: O(1)
+isEmpty: O(1)
+Enqueu: O(1)
+Dequeue: O(1)
+
+## Solution
+![whiteboard](stacksqueues.png)
